f5fb884229829b419208c0ce5cc0d7816c9f6813
alex <yukhymenko.oleksandr@gmail.com>
2017-01-26 13:02:42 +0200

    implement horizontal blur

--- parprog1/scalashop/src/main/scala/scalashop/HorizontalBoxBlur.scala
+++ parprog1/scalashop/src/main/scala/scalashop/HorizontalBoxBlur.scala
@@ -37,26 +37,34 @@ object HorizontalBoxBlurRunner {
 object HorizontalBoxBlur {
 
   /** Blurs the rows of the source image `src` into the destination image `dst`,
-   *  starting with `from` and ending with `end` (non-inclusive).
-   *
-   *  Within each row, `blur` traverses the pixels by going from left to right.
-   */
-  def blur(src: Img, dst: Img, from: Int, end: Int, radius: Int): Unit = {
-  // TODO implement this method using the `boxBlurKernel` method
-
-  ???
+    *  starting with `from` and ending with `end` (non-inclusive).
+    *
+    *  Within each row, `blur` traverses the pixels by going from left to right.
+    */
+  def blur(src: Img, dst: Img, from: Int, end: Int, radius: Int): Unit =
+  for {
+    y <- from until end
+    x <- 0 until src.width
+  } {
+    dst(x, y) = boxBlurKernel(src, x, y, radius)
   }
 
   /** Blurs the rows of the source image in parallel using `numTasks` tasks.
-   *
-   *  Parallelization is done by stripping the source image `src` into
-   *  `numTasks` separate strips, where each strip is composed of some number of
-   *  rows.
-   */
+    *
+    *  Parallelization is done by stripping the source image `src` into
+    *  `numTasks` separate strips, where each strip is composed of some number of
+    *  rows.
+    */
   def parBlur(src: Img, dst: Img, numTasks: Int, radius: Int): Unit = {
-  // TODO implement using the `task` construct and the `blur` method
-
-  ???
+    val height = src.height
+    val step = Math.max(height / numTasks, 1)
+    (0 until height by step).map { y =>
+      val maybeEnd = y + step
+      val end = if (maybeEnd > height) height else maybeEnd
+      task {
+        blur(src, dst, y, end, radius)
+      }
+    }.foreach(_.join())
   }
 
 }
