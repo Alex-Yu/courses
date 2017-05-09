83374d73e8acb0729caa35a5dd78d7afa9542e12
alex <yukhymenko.oleksandr@gmail.com>
2017-05-09 18:21:28 +0300

    implement locateTemperatures

--- scala-capstone/observatory/src/main/scala/observatory/Extraction.scala
+++ scala-capstone/observatory/src/main/scala/observatory/Extraction.scala
@@ -1,11 +1,29 @@
 package observatory
 
+import java.nio.file.Paths
 import java.time.LocalDate
+
+import org.apache.spark.sql.{DataFrame, Row, SparkSession}
 
 /**
   * 1st milestone: data extraction
   */
+
+
 object Extraction {
+
+  val spark: SparkSession =
+    SparkSession
+      .builder()
+      .appName("Capstone")
+      .config("spark.master", "local")
+      .getOrCreate()
+
+  import spark.implicits._
+
+  private def toCelsius(fahrenheitT: Double): Double = (fahrenheitT - 32) / 1.8
+  case class Station(stn: String, wban: String, lat: Double, lon: Double)
+  case class TempByDay(stn: String, wban: String, month: Int, day: Int, celsiusT: Double)
 
   /**
     * @param year             Year number
@@ -14,7 +32,35 @@ object Extraction {
     * @return A sequence containing triplets (date, location, temperature)
     */
   def locateTemperatures(year: Int, stationsFile: String, temperaturesFile: String): Iterable[(LocalDate, Location, Double)] = {
-    ???
+    def fsPath(resource: String): String = Paths.get(getClass.getResource(resource).toURI).toString
+//    def toOption[T](s: String, f: String => T): Option[T] = if (s.isEmpty) None else Some(f(s))
+
+    val stationsDf: DataFrame = spark.sparkContext.textFile(fsPath(stationsFile))
+      .map(_.split(","))
+      .collect {
+        case attr if attr.length >= 4 && attr.forall(_.nonEmpty) => Station(attr(0), attr(1), attr(2).trim.toDouble, attr(3).trim.toDouble)
+      }.toDF()
+
+    stationsDf.createOrReplaceTempView("stations")
+
+    val tempByDayDF = spark.sparkContext.textFile(fsPath(temperaturesFile))
+      .map(_.split(","))
+      .collect {
+        case attr if attr.length >= 5 && attr.forall(_.nonEmpty) && attr(4).trim != "9999.9" =>
+          TempByDay(attr(0), attr(1), attr(2).trim.toInt, attr(3).trim.toInt, toCelsius(attr(4).trim.toDouble))
+      }.toDF()
+
+    tempByDayDF.createOrReplaceTempView("tempByDay")
+
+    stationsDf.join(
+      tempByDayDF, stationsDf.col("stn") === tempByDayDF.col("stn") && stationsDf.col("wban") === tempByDayDF.col("wban")
+    ).select($"month", $"day", $"lat", $"lon", $"celsiusT")
+      .collect
+    .map(row => (
+      LocalDate.of(year, row.getInt(0), row.getInt(1)),
+      Location(row.getDouble(2), row.getDouble(3)),
+      row.getDouble(4)
+    ))
   }
 
   /**
